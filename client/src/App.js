ae7d5f9fa31bef94ed83e3e829b34b66fee8d20d
Xelanor <berozel@hotmail.com>
2019-08-30 22:51:18 +0300

    Authentication added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store/store'
 
 import HomePage from './containers/HomePage'
 import Navbar from './components/Layout/Navbar/Navbar'
+import Register from './containers/Auth/Register'
+import Login from './containers/Auth/Login'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" exact component={HomePage} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route path="/" exact component={HomePage} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 require('dotenv').config();
 
@@ -9,6 +11,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const uri = process.env.ATLAS_URI
 
