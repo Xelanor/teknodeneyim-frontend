26e20200790860b7c1ae97e7e97488f553022341
Xelanor <berozel@hotmail.com>
2019-08-30 07:34:32 +0300

    Posting an entry

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class HomePage extends Component {
-  state = {}
+  state = {
+    username: "",
+    content: ""
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onFormSubmitHandler = e => {
+    e.preventDefault()
+
+    const entry = {
+      username: this.state.username,
+      content: this.state.content
+    }
+
+    axios.post('entries/add', entry)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
-      <div>
+      <div style={{ width: '50%', margin: '0 auto' }}>
         <h1>Hello World!</h1>
+        <form onSubmit={this.onFormSubmitHandler}>
+          <label for="name">Name</label>
+          <input className="u-full-width" type="text" id="name" name="username" onChange={this.handleChange}></input>
+          <label for="content">Content</label>
+          <textarea className="u-full-width" type="text" id="content" name="content" onChange={this.handleChange}></textarea>
+          <button className="button-primary">Konu Yarat</button>
+        </form>
       </div>
     );
   }
