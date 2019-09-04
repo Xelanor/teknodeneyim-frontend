db0025237811e9b2186157401c4bc3978a4f9141
Xelanor <berozel@hotmail.com>
2019-09-04 22:57:46 +0300

    Version 0.1.0

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.use('/comments', commentsRouter);
 // Serve static assets if in PROD
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('/../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
   })
 }
 
