272329195049c2558758a6ee259ac510c66006c4
Xelanor <berozel@hotmail.com>
2019-09-08 21:41:51 +0300

    New Colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        tekno: '#F67E7D'
+        tekno: '#F67E7D',
+        tekno2: '#FFB997',
+        tekno3: '#843B62',
+        tekno4: '#0B032D',
+        tekno5: '#545454'
       }
     }
   },
