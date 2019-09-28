b511ef50601fa0aa128df3424941484894bc6771
Xelanor <berozel@hotmail.com>
2019-09-28 21:32:52 +0300

    Sidebar toggle animation

--- client/src/containers/Sidebar/Sidebar.js
+++ client/src/containers/Sidebar/Sidebar.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { CSSTransition } from 'react-transition-group';
+import './Sidebar.css'
 
 import PostsList from '../../components/PostsList/PostsList'
 
 import { fetchSidePosts } from '../../store/actions/fetchActions'
 
 class Sidebar extends Component {
-  state = {}
+  state = {
+    displaySidebar: true,
+  }
 
   componentDidMount() {
     this.props.fetchSidePosts()
   }
 
+  toggleSidebar = () => {
+    this.setState({ displaySidebar: !this.state.displaySidebar })
+  }
+
   render() {
     let posts
     if (this.props.posts) {
-      posts = <PostsList posts={this.props.posts} />
+      posts =
+        <div>
+          <div className="flex justify-between">
+            <div className="mb-4 font-semibold text-base text-gray-900">
+              En Popüler Deneyimler
+            </div>
+          </div>
+          <PostsList posts={this.props.posts} />
+        </div>
     }
     return (
-      <div className="md:w-3/12 p-4 border-r-2 border-gray-200">
-        <div className="flex justify-between">
-          <div className="mb-4 font-semibold text-base text-gray-900">
-            En Son Gelişmeler
-            </div>
-          {/* <div className="font-semibold text-base text-gray-900">
-            #Popüler
-            </div> */}
+      <div className="md:w-3/12 px-4 pb-4 lg:p-4 border-r-2 border-gray-200">
+        <button onClick={this.toggleSidebar} className="bg-tekno lg:hidden hover:bg-tekno mb-2 text-white font-bold h-6 w-full rounded focus:outline-none focus:shadow-outline appearance-none" type="submit">
+          <i className="fas fa-chevron-down"></i>
+        </button>
+        <div>
+          <CSSTransition
+            in={this.state.displaySidebar}
+            timeout={300}
+            classNames="alert"
+            unmountOnExit
+          >
+            <>
+              {posts}
+            </>
+          </CSSTransition>
         </div>
-        <>
-          {posts}
-        </>
       </div>
     );
   }
