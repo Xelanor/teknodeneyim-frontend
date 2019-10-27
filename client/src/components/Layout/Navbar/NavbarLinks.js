2eb646e0f4197bb8b1bb9a104ebe9833377908cd
Xelanor <berozel@hotmail.com>
2019-10-27 20:23:12 +0300

    NavbarLinks font size

--- client/src/components/Layout/Navbar/NavbarLinks.js
+++ client/src/components/Layout/Navbar/NavbarLinks.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import './NavbarLinks.css'
+
 const navbarLinks = (props) => {
   return (
     <>
-      <li className="text-tekno3 md:text-xs lg:text-base font-bold" style={{ fontSize: '0.6rem' }}>
+      <li className="text-tekno3 md:text-xs lg:text-base font-bold navbar-links">
         <Link to={`/search/${props.to}`} title={props.title}>#{props.content}</Link>
       </li>
     </>
