ec574f6797714233a399d86baac83d4cfc54082d
Xelanor <44201710+Xelanor@users.noreply.github.com>
2019-11-10 17:50:21 +0300

    Telegram integration

--- routes/reports.js
+++ routes/reports.js
@@ -2,18 +2,14 @@ const router = require('express').Router();
 const mailer = require('../utils/Mailer')
 let Report = require('../models/report');
 let User = require('../models/user');
+const Telegram = require('../utils/Telegram')
 
 router.route('/new-report').post((req, res) => {
   const { reporter, reported, comment } = req.body
   const newReport = new Report({ reporter, reported, comment });
 
   newReport.save()
-    .then(() => mailer.send({
-      to: "berkeozelsel@gmail.com",
-      from: 'info@teknodeneyim.com',
-      subject: 'TeknoDeneyim Şifre Sıfırlama',
-      text: `Rapor Geldi`
-    }))
+    .then(() => Telegram(`Rapor geldi`))
     .catch(err => res.status(400).json('Error: ' + err));
 
   User.findOneAndUpdate({ _id: reporter }, {
